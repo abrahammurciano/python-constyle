a7d3731b60a7f8d0c8a61043ec95604ee3e09e81
Abraham Murciano <abrahammurciano@gmail.com>
2024-06-03 22:05:20 +0300

    Remove __version__

--- constyle/__init__.py
+++ constyle/__init__.py
@@ -2,15 +2,6 @@
 .. include:: ../README.md
 """
 
-import sys
-
-if sys.version_info >= (3, 8):
-    from importlib import metadata
-else:
-    import importlib_metadata as metadata
-
-__version__ = metadata.version(__package__ or __name__)
-
 from ._style import Style, style
 from ._attributes import Attributes
 
